f855f45c796c866dcb9dfa9e83eae46ab5fae7db
Ramsey <rdarling42@gmail.com>
2016-05-23 16:00:43 -0400

    working addition and subtraction, added gears and hiding phases

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 $(document).foundation();
+
+$(document).ready(function(){
+
+    $('.phase-trigger').click(function(){
+        var phase = $(this).attr('data-phase');
+        $('.phase-'+phase+' .component-wrap').slideToggle();
+    });
+
+    $('.switch-input').click(function(){
+        var issue_type = $(this).attr('data-issue-type');
+        var high = $('.high-end').html();
+        var low = $('.low-end').html();
+        switch(issue_type){
+            case 'small':
+                if($(this).is(':checked')){
+                    high = Number(high) + 2;
+                    low = Number(low) + 1;
+                }else{
+                    high = Number(high) - 2;
+                    low = Number(low) - 1;
+                }
+                break;
+            case 'med':
+                if($(this).is(':checked')){
+                    high = Number(high) + 5;
+                    low = Number(low) + 3;
+                }else{
+                    high = Number(high) - 5;
+                    low = Number(low) - 3;
+                }
+                break;
+            case 'large':
+                if($(this).is(':checked')){
+                    high = Number(high) + 8;
+                    low = Number(low) + 6;
+                }else{
+                    high = Number(high) - 8;
+                    low = Number(low) - 6;
+                }
+                break;
+        }
+        $('.high-end').html(high);
+        $('.low-end').html(low);
+    });
+});
